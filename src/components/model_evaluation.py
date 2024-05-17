729398a53e100bfbb133c502a16f46a7f7c27778
srinivaspadhy123 <srinivaspadhybm@gmail.com>
2024-05-17 20:12:15 +0530

    added mlflow in model evaluation script

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import mlflow
 from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
 from urllib.parse import urlparse
 import mlflow
@@ -12,20 +13,40 @@ from src.logger.logging import logging
 from src.exceptions.exception import CustomException
 from dataclasses import dataclass
 
-@dataclass
-class ModelEvaluationConfig:
-    pass
+
 
 class ModelEvaluation:
     def __init__(self):
-        pass
+        logging.info("evaluation started")
 
-    def initiate_model_evaluation(self):
+    def eval_metrics(self,actual,pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual,pred)
+        r2 = r2_score(actual,pred)
+        logging.info("Evaluation Metrics Captured")
+        return rmse,mae,r2
+    
+    def initiate_model_evaluation(self,train_array,test_array):
         try:
-            pass
+            X_test,y_test = (test_array[:,:-1],test_array[:,-1])
+            model_path=os.path.join("artifacts","model.pkl")
+            model = load_object(model_path)
+            # mlflow.set_registry_uri("")
+            logging.info("Model has registered")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            print(tracking_url_type_store)
+
+            with mlflow.start_run():
+                prediction = model.predict(X_test)
+                (rmse,mae,r2) = self.eval_metrics(y_test,prediction)
+                mlflow.log_metric("mae",mae)
+                mlflow.log_metric("rmse",rmse)
+                mlflow.log_metric("r2_score",r2)
+                if tracking_url_type_store != 'file':
+                    mlflow.sklearn.log_model(model,"model",registered_model_name="ml_model")
+                else:
+                    mlflow.sklearn.log_model(model,"model")
+
         except Exception as exp:
-            logging.info()
             raise CustomException(exp,sys)
-
-
 
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -7,6 +7,7 @@ import pandas as pd
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
+from src.components.model_evaluation import ModelEvaluation
 
 obj = DataIngestion()
 train_data_path,test_data_path = obj.initiate_data_ingesiton()
@@ -15,3 +16,5 @@ data_transformation = DataTransformation()
 train_arr,test_arr = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initiate_model_training(train_arr,test_arr)
+model_evaluation_obj = ModelEvaluation()
+model_evaluation_obj.initiate_model_evaluation(train_arr,test_arr)
